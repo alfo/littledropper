525496fd3d2220a6d0797e1982affa2f99645fe0
Alex Forey <me@alexforey.com>
2013-03-02 15:06:49 +0000

    whoops

--- public/app.js
+++ public/app.js
@@ -2,26 +2,26 @@ $(function() {
 
 	$('#dropzone').click(function() {
 		text = prompt("Send to printer:", "");
-		if (text !== "") send(text);
+		send(text);
 	});
 });
 
 function send(text) {
-		$.ajax({
-			type: "POST",
-			url: "/post",
-			data: {
-				text: text
-			},
-			cache: false,
-			complete: function(jqXHR, textStatus) {
-				switch (jqXHR.status) {
-					case 200:
-						alert("Sent to Printer");
-						break;
-					default:
-						alert("Something went wrong.");
-				}
+	$.ajax({
+		type: "POST",
+		url: "/post",
+		data: {
+			text: text
+		},
+		cache: false,
+		complete: function(jqXHR, textStatus) {
+			switch (jqXHR.status) {
+				case 200:
+					alert("Sent to Printer");
+					break;
+				default:
+					alert("Something went wrong.");
 			}
-		});
-	}
+		}
+	});
+}
